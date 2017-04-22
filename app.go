d754358c1a175b1f0aba74da5e2acc617e01ff3f
dominic <dominic.tracey@gmail.com>
2017-04-22 14:47:23 -0400

    irritating packaging in go

--- app.go
+++ app.go
@@ -64,6 +64,28 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
+// Pilot represents a drone pilot
+type Pilot struct {
+	ID       int64
+	Name     string
+	Licensed bool
+	Address  string
+	Phone    string
+}
+
+// NewPilot returns a new Pilot instance
+func NewPilot() *Pilot {
+	var i = new(Pilot)
+
+	//i.Id = "a.assign(metadata.InstanceID)"
+	i.Name = "Fred Smith"
+	i.Licensed = true
+	i.Address = "98 Wallaby Way, Sydney AUS"
+	i.Phone = "+23 0903 91203"
+
+	return i
+}
+
 func pilotGetHandler(w http.ResponseWriter, r *http.Request) {
 	pilot := model.NewPilot()
 	pilot.Address = "123 Wallaby Way"
